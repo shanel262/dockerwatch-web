5483e2bb527d12ff57fa06ccc311c99c4fab9c95
Shane Lacey <shanel262@hotmail.com>
2017-01-29 18:47:06 +0000

    Reinserted projects view route, nav header link, controller and service

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
 
   app.use('/api/stats', require('./api/stats/index'));
+  app.use('/api/projects', require('./api/projects/index'));
   
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|app|assets)/*')
